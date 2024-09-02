cbe01e284c0d96f4fddc936ff7064dd5fdd6db90
Ankush Sahu <152798294+Ankush-sahu-123@users.noreply.github.com>
2024-09-02 13:09:26 +0530

    Update About.jsx

--- front-end/src/pages/About/About.jsx
+++ front-end/src/pages/About/About.jsx
@@ -3,7 +3,14 @@ import "./About.css"
 
 const About = () => {
   return (
-    <div>About</div>
+   <div className="about">
+        <div className="about-name">About:</div>
+        <div className="about-content">
+            <p className="tagline">Preparing for Dreams need Peace</p>
+            <h1 className="about-details">At Dunction, we transform the way you work by offering dynamic, flexible coworking spaces designed to inspire creativity and collaboration. Join us to experience a vibrant community and top-notch amenities tailored to your professional needs.</h1>
+            <div className="images"></div>
+        </div>
+    </div>
   )
 }
 
